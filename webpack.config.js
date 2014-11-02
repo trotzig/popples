37969221a894f44f2e57eb0dc3d37f9eaabcac57
Henric Trotzig <henric.trotzig@causes.com>
2014-11-02 10:16:06 +0100

    Fix indentation in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    entry: "./_feed-the-blob.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "feed-the-blob.js"
-    },
-    resolve: {
-      modulesDirectories: [
-        __dirname + '/app'
-      ]
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'es6-loader'
-        }
-      ]
-    }
+  entry: "./_feed-the-blob.js",
+  output: {
+      path: __dirname + "/dist",
+      filename: "feed-the-blob.js"
+  },
+  resolve: {
+    modulesDirectories: [
+      __dirname + '/app'
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'es6-loader'
+      }
+    ]
+  }
 };
