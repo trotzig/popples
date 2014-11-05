9a836f0095a24af4a694f478ff9b62873bfb37d3
Henric Trotzig <henric.trotzig@causes.com>
2014-11-05 20:13:26 +0100

    Make bubbles appear at the bottom, like a bubble

--- app/objects/bubble.js
+++ app/objects/bubble.js
@@ -25,16 +25,22 @@ class Bubble {
   }
 
   bubbleFromBottom(game, posX, speedModifier) {
+    this.bubble.scale(0.1);
     this.bubble.x = posX;
-    this.bubble.y = game.height;
+    this.bubble.y = game.height - this.size;
 
     // Burst on touch/click
     this.bubble.addEventListener('touchstart',  this.burst.bind(this, true));
 
-    // Wiggle it
-    this.bubble.tl.moveBy(-this.size * 0.4, 0, this.size, einout)
-                  .moveBy(this.size * 0.4, 0, this.size, einout)
-                  .loop();
+    // Make it appear
+    this.bubble.tl.scaleTo(1, 8, eout).and()
+                  .moveBy(0, -this.size * 0.5, 8, eout)
+                  .then(() => {
+      // Wiggle it
+      this.bubble.tl.moveBy(-this.size * 0.4, 0, this.size, einout)
+                    .moveBy(this.size * 0.4, 0, this.size, einout)
+                    .loop();
+    });
 
     // Bubble it towards the top
     game.addEventListener('enterframe', this.moveUp.bind(this, speedModifier));
